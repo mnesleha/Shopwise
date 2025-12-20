6f5b2e7d7fff2ca5ae0c2b736150c42b2781d60b
Marek Nesleha <mnesleha@gmail.com>
2025-12-20 19:11:10 +0100

    MySQL setup

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -1,4 +1,8 @@
+import os
+from dotenv import load_dotenv
 from .base import *
+
+load_dotenv()
 
 DEBUG = True
 
@@ -7,14 +11,28 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "shopwise",
-        "USER": "root",
-        "PASSWORD": "Maxipes.",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "3306"),
         "OPTIONS": {
             "charset": "utf8mb4",
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "shopwise",
+#         "USER": "root",
+#         "PASSWORD": "Maxipes.",
+#         "HOST": "localhost",
+#         "PORT": "3306",
+#         "OPTIONS": {
+#             "charset": "utf8mb4",
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
